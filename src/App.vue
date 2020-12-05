14358dcdceb3895e5df90faa0cb9b44739663181
Zubs <zubairidrisaweda@gmail.com>
2020-12-06 00:39:06 +0100

    Tried routing again

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,62 @@
 <template>
   <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <template>
-      <v-card class="overflow-hidden">
-        <v-app-bar
-          absolute
-          color="#6A76AB"
-          dark
-          shrink-on-scroll
-          prominent
-          src="https://picsum.photos/1920/1080?random"
-          fade-img-on-scroll
-          scroll-target="#scrolling-techniques-3"
-        >
-          <template v-slot:img="{ props }">
-            <v-img
-              v-bind="props"
-              gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
-            ></v-img>
-          </template>
+    <v-app>
+      <template>
+        <v-card class="overflow-hidden">
+          <v-app-bar
+            absolute
+            color="#6A76AB"
+            dark
+            shrink-on-scroll
+            prominent
+            src="https://picsum.photos/1920/1080?random"
+            fade-img-on-scroll
+            scroll-target="#scrolling-techniques-3"
+          >
+            <template v-slot:img="{ props }">
+              <v-img
+                v-bind="props"
+                gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+              ></v-img>
+            </template>
 
-          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-          <v-toolbar-title>Title</v-toolbar-title>
+            <v-toolbar-title>Title</v-toolbar-title>
 
-          <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
 
-          <v-btn icon>
-            <v-icon>mdi-heart</v-icon>
-          </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
 
-          <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
 
-          <template v-slot:extension>
-            <v-tabs align-with-title>
-              <v-tab>Tab 1</v-tab>
-              <v-tab>Tab 2</v-tab>
-              <v-tab>Tab 3</v-tab>
-            </v-tabs>
-          </template>
-        </v-app-bar>
-        <v-sheet
-          id="scrolling-techniques-3"
-          class="overflow-y-auto"
-          max-height="600"
-        >
-          <v-container style="height: 1000px;"></v-container>
-        </v-sheet>
-      </v-card>
-    </template>
-    <router-view/>
+            <template v-slot:extension>
+              <v-tabs align-with-title>
+                <v-tab>Tab 1</v-tab>
+                <v-tab>Tab 2</v-tab>
+                <v-tab>Tab 3</v-tab>
+              </v-tabs>
+            </template>
+          </v-app-bar>
+          <v-sheet
+            id="scrolling-techniques-3"
+            class="overflow-y-auto"
+          >
+            <v-container>
+              <router-view/>
+            </v-container>
+          </v-sheet>
+        </v-card>
+      </template>
+    </v-app>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contact from '../views/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +12,28 @@ const routes = [
     component: Home
   },
   {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/auth/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "login" */ '../views/auth/Register.vue')
+  },
 ]
 
 const router = new VueRouter({
