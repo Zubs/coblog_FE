f29b712766cbfe336ec43c440a12209b1932ec19
Zubs <zubairidrisaweda@gmail.com>
2020-12-08 17:30:05 +0100

    Trying to revert now

--- src/views/auth/Login.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<v-app class="pink lighten-4">
-		<h1>Login</h1>
-	</v-app>
-</template>
--- src/views/auth/Register.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<v-app class="pink lighten-4">
-		<h1>Register</h1>
-	</v-app>
-</template>
